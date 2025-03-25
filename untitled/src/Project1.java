38615aa305d75a10acc5e1ec7d8f478d7c00d8bd
cho eun jae <jej059813@naever.com>
2025-03-25 11:40:37 +0900

    untitled 파일 추가

---
+++ untitled/src/Project1.java
@@ -0,0 +1,7 @@
+public class Project1 {
+    public static void main(String[] args) {
+        String name = "조은재";
+        int age = 24;
+        System.out.println(name + "님은 " + age + "살 입니다.");
+    }
+}
---
+++ untitled/src/Project2.java
@@ -0,0 +1,7 @@
+public class Project2 {
+    public static void main(String[] args) {
+        int radius = 10;
+        final double PI = 3.14;
+        System.out.println("반지름이" + radius + "인 원의 면적은" + radius * radius * PI + "이다.");
+    }
+}
