5f063d7e78d5782a7133104f8bb9eb11f99a12ff
cho eun jae <jej059813@naever.com>
2025-04-18 13:51:40 +0900

    카페음료 가격 계산 프로그램 추가

--- untitled/src/GradeCalculator.java
+++ untitled/src/GradeCalculator.java
@@ -4,7 +4,7 @@ public class GradeCalculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("점수를 입력하세요: ");
+        System.out.println("점수를 입력하세요: ");
         int score = scanner.nextInt();
         String grade;
 
